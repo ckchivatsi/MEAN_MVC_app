659b224a98dc88b32e4f0e51e58e660b7eae3b6b
ckchivatsi <kelvinchivatsi@hotmail.com>
2017-02-16 11:21:55 +0300

    passport-facebook and passport-twitter authentication

--- config/env/development.js
+++ config/env/development.js
@@ -2,5 +2,15 @@ var port = 1337;
 
 module.exports = {
     port: port,
-    db: 'mongodb://localhost/todos'
+    db: 'mongodb://localhost/todos',
+    facebook: {
+        clientID: '1923494037884586',
+        clientSecret: 'b15d68bdb6da9f76e4ebee2529a38350',
+        callbackURL: 'http://localhost:'+ port +'/oauth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'ZKV9P0uo7rbzFoqwO3GwVtbsj',
+        clientSecret: '4mEB6atmpzPLwS7y34G5koxZkEdtwtzOUf6e1b4Dy5vx7V8MQB',
+        callbackURL: 'http://localhost:1337/oauth/twitter/callback'
+    }
 };
