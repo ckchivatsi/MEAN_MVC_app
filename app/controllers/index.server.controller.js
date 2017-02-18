c541c087a433dfe4ade8b4ee19ba711479e39bd2
ckchivatsi <kelvinchivatsi@hotmail.com>
2017-02-18 11:39:26 +0300

    Fixed issue with getting user name

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,6 +1,7 @@
 exports.render = function(req, res) {
     res.render('index', {
     	title: 'MEAN MVC',
-    	user: req.user ? req.user.username : ''
+    	user: req.user ? req.user.username : 'Guest'
     });
+    console.log(req.user ? req.user.username: 'Guest');
 };
--- config/express.js
+++ config/express.js
@@ -1,25 +1,25 @@
 var config = require('./config'),
     express = require('express'),
+    bodyParser = require('body-parser'),
     passport = require('passport'),
     flash = require ('connect-flash'),
     session = require('express-session');
-    bodyParser = require('body-parser');
 
 module.exports = function() {
     var app = express();
 
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(flash());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
     app.use(session({
         saveUninitialized: true,
         resave: true,
         secret: 'OurSuperSecretCookieSecret'
     }));
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
