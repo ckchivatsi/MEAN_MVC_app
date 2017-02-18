7ae277a1b0859587326aeef06ee1e7b590e6ab1d
ckchivatsi <kelvinchivatsi@hotmail.com>
2017-02-18 17:16:15 +0300

    angularJS - finalizing the todos app... so many errors and bug though

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,7 +1,7 @@
 exports.render = function(req, res) {
     res.render('index', {
     	title: 'MEAN MVC',
-    	user: req.user ? req.user.username : 'Guest'
+    	user: JSON.stringify(req.user)
     });
-    console.log(req.user ? req.user.username: 'Guest');
+    console.log(JSON.stringify(req.user));
 };
